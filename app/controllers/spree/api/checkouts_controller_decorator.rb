02482b1882a262ae1a234852e9a60e4ff1ba9fba
Vishal Zambre <v.zambre@gmail.com>
2016-11-04 22:40:32 +0530

    lock order for update

--- app/controllers/spree/api/checkouts_controller_decorator.rb
+++ app/controllers/spree/api/checkouts_controller_decorator.rb
@@ -6,10 +6,10 @@ module Spree
       private
 
       def remove_payment_attributes
-        @order = Spree::Order.lock(lock).find_by!(number: params[:id])
+        @order = Spree::Order.lock(true).find_by!(number: params[:id])
         return unless @order.flat_percent_order?
         params.delete(:payment_source)
-        params[:order].delete(:payments_attributes)
+        params[:order].delete(:payments_attributes) if params[:order]
       end
     end
   end
